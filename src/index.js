68f9124eae8e775d86442d38b112a9765ae04fbb
emilgelm <emil.gelman@gmail.com>
2019-10-14 18:16:12 +0300

    added cors

--- src/index.js
+++ src/index.js
@@ -7,12 +7,16 @@ const cors = require('cors');
 const jwt = require('./utils/jwt');
 const config = require('./config');
 const errorHandler = require('./utils/ErrorHandler');
+const bodyParser = require('body-parser');
 
 const start = async () => {
 
     try {
         const app = express();
-        const port = 3000;
+        const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
+        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(bodyParser.json());
+        app.use(cors());
         app.use(routes);
         app.use(jwt());
         app.use(errorHandler);
