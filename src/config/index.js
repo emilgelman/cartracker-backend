199a6e5e179576da2088e7b0230c8a23a596c6c9
emilgelm <emil.gelman@gmail.com>
2019-10-24 23:53:41 +0300

    run twice an hour

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongo: 'mongodb+srv://Admin:JFFIRetui4fs2i9K@cluster0-zzpij.mongodb.net/cars?retryWrites=true&w=majority',
-    secret: "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING"
+    secret: "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING",
+    schedule: '* * * * *'
 };
